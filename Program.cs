8c7d4b5887e1f562116219701a5f026a1b2347b1
David Barrett <39411621+David-Barrett-MS@users.noreply.github.com>
2023-03-24 12:05:05 +0000

    Improve comments

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * By David Barrett, Microsoft Ltd. 2021. Use at your own risk.  No warranties are given.
+ * By David Barrett, Microsoft Ltd. 2022-2023. Use at your own risk.  No warranties are given.
  * 
  * DISCLAIMER:
  * THIS CODE IS SAMPLE CODE. THESE SAMPLES ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND.
@@ -22,11 +22,23 @@ namespace IMAPOAuthSample
 {
     class Program
     {
-
+        /// <summary>
+        /// TcpClient for the IMAP connection
+        /// </summary>
         private static TcpClient _imapClient = null;
+        /// <summary>
+        /// SSLStream for communicating with IMAP server
+        /// </summary>
         private static SslStream _sslStream = null;
+        /// <summary>
+        /// IMAP server address
+        /// </summary>
         private static string _imapEndpoint = "outlook.office365.com";
 
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
         static void Main(string[] args)
         {
             if (args.Length<2)
@@ -37,17 +49,24 @@ namespace IMAPOAuthSample
 
             Task task = null;
             if (args.Length > 2 && !String.IsNullOrEmpty(args[2]) && !String.IsNullOrEmpty(args[3]))
-            {
                 // Application auth
                 task = TestIMAP(args[1], args[0], args[2], args[3]);
-            }
             else
+                /// Interactive (delegated) auth
                 task = TestIMAP(args[1], args[0]);
 
             task.Wait();
         }
 
 
+        /// <summary>
+        /// Test the given provided IMAP details (attempt to obtain token and access mailbox)
+        /// </summary>
+        /// <param name="ClientId">Application/Client Id</param>
+        /// <param name="TenantId">Tenant Id</param>
+        /// <param name="SecretKey">Required for app auth</param>
+        /// <param name="mailbox">Required for app auth</param>
+        /// <returns>Task (no data returned, task runs until completion)</returns>
         static async Task TestIMAP(string ClientId, string TenantId, string SecretKey=null, string mailbox=null)
         {
             var imapScope = new string[] { $"https://{_imapEndpoint}/IMAP.AccessAsUser.All" };
@@ -123,6 +142,10 @@ namespace IMAPOAuthSample
             Console.WriteLine("Finished");
         }
 
+        /// <summary>
+        /// Read the server response from the active SSL stream
+        /// </summary>
+        /// <returns>Server response as a string</returns>
         static string ReadSSLStream()
         {
             int bytes = -1;
@@ -133,6 +156,10 @@ namespace IMAPOAuthSample
             return response;
         }
 
+        /// <summary>
+        /// Write the supplied data to the active SSL stream
+        /// </summary>
+        /// <param name="Data">The string data to be written</param>
         static void WriteSSLStream(string Data)
         {
             _sslStream.Write(Encoding.ASCII.GetBytes($"{Data}{Environment.NewLine}"));
@@ -140,6 +167,11 @@ namespace IMAPOAuthSample
             Console.WriteLine(Data);
         }
 
+        /// <summary>
+        /// Connect to a mailbox using the provided authentication information and show the count of unread messages
+        /// </summary>
+        /// <param name="authResult">Valid OAuth credentials to access the mailbox</param>
+        /// <param name="mailbox">Mailbox to be accessed (required for app only flow)</param>
         static void RetrieveMessages(AuthenticationResult authResult, string mailbox = null)
         {
             try
@@ -183,10 +215,14 @@ namespace IMAPOAuthSample
             }
         }
 
+        /// <summary>
+        /// Calculate and return the log-in code, which is a base 64 encoded combination of mailbox (user) and auth token
+        /// </summary>
+        /// <param name="authResult">Valid OAuth token</param>
+        /// <param name="mailbox">If missing, mailbox will be read from the token</param>
+        /// <returns>IMAP log-in code</returns>
         static string XOauth2(AuthenticationResult authResult, string mailbox = null)
         {
-            // Create the log-in code, which is a base 64 encoded combination of mailbox (user) and auth token
-
             char ctrlA = (char)1;
             if (String.IsNullOrEmpty(mailbox))
                 mailbox = authResult.Account.Username;
